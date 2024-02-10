328cb0b9d1b1060167e14201fc2637c250e6a2db
Bryce Russell <brycetrussell@gmail.com>
2024-02-10 12:40:48 -0600

    Add astro-integration-kit plugin

---
+++ package/plugins/astro-integration-kit.ts
@@ -0,0 +1,24 @@
+import type { Option } from "../types";
+import { definePlugin } from "astro-integration-kit";
+import addPageDir from "../utils/add-page-dir";
+
+export default definePlugin({
+  name: "addPageDir",
+  hook: "astro:config:setup",
+  implementation:
+    ({ config, logger, injectRoute }) => {
+      return (option: string | Option) => {
+        if (typeof option === "string") {
+          option = {
+            dir: option
+          }
+        }
+        addPageDir({
+          ...option,
+          config,
+          logger,
+          injectRoute
+        })
+      }
+    }
+});
