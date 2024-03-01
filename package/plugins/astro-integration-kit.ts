e0ed6c4997e058817bc7e303a6eb3f0d11ce5df2
Bryce Russell <brycetrussell@gmail.com>
2024-02-29 19:10:51 -0600

    Add Changesets and Linting (#13)
    * Install dep for changesets and linting
    
    - Install dep for changesets and linting
    - Add scripts
    
    * Add changesets action
    
    * lint
    
    * Add LICENSE
    
    * Initialize changesets cli
    
    * add changeset

--- package/plugins/astro-integration-kit.ts
+++ package/plugins/astro-integration-kit.ts
@@ -1,24 +1,23 @@
-import type { Option } from "../types";
 import { definePlugin } from "astro-integration-kit";
+import type { Option } from "../types";
 import addPageDir from "../utils/add-page-dir";
 
 export default definePlugin({
-  name: "addPageDir",
-  hook: "astro:config:setup",
-  implementation:
-    ({ config, logger, injectRoute }) => {
-      return (option: string | Option) => {
-        if (typeof option === "string") {
-          option = {
-            dir: option
-          }
-        }
-        return addPageDir({
-          ...option,
-          config,
-          logger,
-          injectRoute
-        })
-      }
-    }
+	name: "addPageDir",
+	hook: "astro:config:setup",
+	implementation: ({ config, logger, injectRoute }) => {
+		return (option: string | Option) => {
+			if (typeof option === "string") {
+				option = {
+					dir: option,
+				};
+			}
+			return addPageDir({
+				...option,
+				config,
+				logger,
+				injectRoute,
+			});
+		};
+	},
 });
